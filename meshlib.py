8e20802ac1db356fefc33f45c56a5188d5a62c1c
frogmane <xzjnh8129@github.com>
2025-01-31 12:59:44 -0500

    qwerty

--- meshlib.py
+++ meshlib.py
@@ -172,3 +172,18 @@ class MeshtasticClient():
         else:
             return (a['position']['latitude'], a['position']['longitude'], 0)
 
+
+if __name__ == "__main__":
+    #from meshlib import *
+    # https://python.meshtastic.org/mesh_interface.html
+
+    client = MeshtasticClient('/dev/ttyACM0')
+    client.meshint.getMyNodeInfo()
+    client.checkMail()
+    client.meshint.sendText("Test", wantAck=True)
+    client.meshint.sendText("DM Test", destinationId='!xxxxxx', wantAck=True)
+    #client.meshint.sendText("Test", channelIndex=0, wantAck=True)
+    client.meshint.sendPosition(latitude=45, longitude=-73, altitude=20, wantAck=True)
+    client.meshint.sendData(b'[TestData]',portNum=258, wantAck=True) #256-511 for private apps
+    client.meshint.sendPosition(latitude=0.0, longitude=0.0, altitude=0, timeSec=0, destinationId='^all', wantAck=False, wantResponse=False)
+    client.meshint.close()
